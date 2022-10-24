918dc0588cbc91f4496c4f1a3fd5ab243dd31461
qqqwwwbbb <usecaut@gmail.com>
2022-10-24 12:27:20 +0700

    admin functions

--- bboard/locallibrary/catalog/admin.py
+++ bboard/locallibrary/catalog/admin.py
@@ -8,3 +8,27 @@ admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
+
+
+# admin.site.register(Author)
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth',
+                    'date_of_death')
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
